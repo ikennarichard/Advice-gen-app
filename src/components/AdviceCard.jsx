c9b33b2657dacfd6f326f1419691d34baa617861
Ikenna Richard <oguejioforrichard@gmail.com>
2023-03-26 12:46:42 -1100

    Add button component

--- src/components/AdviceCard.jsx
+++ src/components/AdviceCard.jsx
@@ -1,13 +1,13 @@
+
 export default function AdviceCard() {
   return (
   <div className="container">
     <h1 className="heading">ADVICE #<span id="advice_id">117</span></h1>
     <p className="advice_text">"Poverty is not a lack of character, poverty is a lack of cash."</p>
     <picture>
-      <source media="(min-width:800px)" srcSet="./images/pattern-divider-desktop.svg"/>
+      <source media="(min-width:800px)" srcSet="./src/images/pattern-divider-desktop.svg"/>
       <img src="./src/images/pattern-divider-mobile.svg" alt="" className="divider"/>
     </picture>
-    <button className="dice_button"><img src="./src/images/icon-dice.svg" alt="" className="dice_image"/></button>
   </div>
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+export default function Button() {
+  return (
+    <button className="dice_button"><img src="./src/images/icon-dice.svg" alt="" className="dice_image"/></button>
+  );
+}
